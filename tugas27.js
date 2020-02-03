319a48e564eeb711ea904d1a8b799a591dbf98cf
R-devMaker <rei.forworkonly@gmail.com>
2020-02-03 14:51:10 +0700

    Tugas 27 JavaScript
    Pengumpulan tugas 27 JavaScript Niomic

---
+++ tugas27.js
@@ -0,0 +1,13 @@
+var no = "No";
+var rumah = "rumah saya";
+var angka = 30;
+var a = "A";
+
+var res = no + " " + rumah + " " + String(angka) + a;
+console.log(res);
+
+var jalan = "Jalan pangeran Tirtayasa No";
+var nomer = 70;
+
+var res2 = jalan + " " +String(nomer);
+console.log(res2);
